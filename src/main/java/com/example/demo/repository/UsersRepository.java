d26d3bb32f4e64221bd4ee735d04cafd22b18258
Minassyants <Killka1997@gmail.com>
2024-08-15 11:32:25 +0500

    123

--- src/main/java/com/example/demo/repository/UsersRepository.java
+++ src/main/java/com/example/demo/repository/UsersRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +8,4 @@ import com.example.demo.entity.Users;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
-  List<Users> findByUsername(String username);
-
-  Users findById(String id);
 }
